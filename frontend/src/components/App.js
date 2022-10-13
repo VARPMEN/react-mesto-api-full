fb27f0aa480f9fbdd1c090f15af056440d858387
Anton Prokhorov <varpmen@gmail.com>
2022-10-13 23:52:48 +0300

    fix/react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
 import Header from "./Header";
 import Login from "./Login";
